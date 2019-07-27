177dc24d0205a81a4753217d414722a0f8df623c
zdobnova <zdobnovad@gmail.com>
2019-07-27 21:52:15 +0700

    lesson2 step1

--- features/step_definitions/area screen steps.rb
+++ features/step_definitions/area screen steps.rb
@@ -1,13 +1,13 @@
 Given(/^I land on help popup$/) do
-  puts ("User lands on help popup")
+  text("Help")
 end
 
 When(/^I click on Got it button$/) do
-  puts ("User clicks on Got it button ")
+  find_element(id:"button1").click
 end
 
 Then(/^I land on Area screen$/) do
-  puts("User lands on Area screen")
+  text("Area")
 end
 
 When(/^I click on Swap button$/) do
