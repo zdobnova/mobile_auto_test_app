08cab3f46af7d9002e2c18f829fa07c25f062c4c
zdobnova <zdobnovad@gmail.com>
2019-07-30 23:34:16 +0700

    Ex3: Add assertions to test

--- features/area_screen.feature
+++ features/area_screen.feature
@@ -1,22 +1,23 @@
 @area
 Feature: User is able to convert area units
-  @wip
-  Scenario: User is able to dismiss  help
-    Given I land on help popup
+
+  Background:
     When I click on Got it button
-    Then I land on Area screen
+
+#  Scenario: User is able to dismiss help
+#    Given I land on help popup
+#    When I click on Got it button
+#    Then I land on Area screen
 
   Scenario: User is able to swap values
-    Given I click on Got it button
-    And I see "Sq Kilometre" in From headers
+    Given I see "Sq Kilometre" in From headers
     And I see "Sq Metre" in to header
     When I click on Swap button
     Then I see "Sq Metre" in From headers
     And I see "Sq Kilometre" in to header
 
   Scenario Outline:  User is able to convert default units
-    Given I click on Got it button
-    And I click on Clear it button
+    Given I click on Clear it button
     When I  enter "<target>" to From field
     Then I get "<result>" i n To field
     Examples:
@@ -24,6 +25,19 @@ Feature: User is able to convert area units
       |1     |1000000|
       |2     |2000000|
       |3     |3000000|
+
+
+  Scenario: User is able to use soft keyboard to enter values
+    Given I click on Clear it button
+    When I click on From field
+    And I press "12" on soft keyboard
+    Then I get "12000000" i n To field
+
+
+    Scenario:
+      When I select "Hectare" from left column
+      Then I see "Hectare" in From headers
+      And I get "10000" i n To field
 
 
 
--- features/step_definitions/area screen steps.rb
+++ features/step_definitions/area screen steps.rb
@@ -1,5 +1,5 @@
 Given(/^I land on help popup$/) do
-  text("Help")
+  find_element(id:"alertTitle")
 end
 
 When(/^I click on Got it button$/) do
@@ -7,29 +7,54 @@ When(/^I click on Got it button$/) do
 end
 
 Then(/^I land on Area screen$/) do
-  text("Area")
+  find_element(xpath: "//android.widget.TextView[@text='Area']")
 end
 
 When(/^I click on Swap button$/) do
-  puts ("User clicks Swap button")
+  find_element(id:"fab").click
 end
 
 Then(/^I see "([^"]*)" in From headers$/) do |value|
-  puts ("Frome header values is"+ value)
+  actual_value = find_element(id:"header_text_unit_from").text
+  if actual_value != value
+    fail ("Expected value is #{value}, but actual value #{actual_value}")
+  end
 end
 
 And(/^I see "([^"]*)" in to header$/) do |value|
-  puts ("To header values is #{value}")
+  actual_value = find_element(id:"header_text_unit_to").text
+  if actual_value != value
+    fail ("Expected value is #{value}, but actual value #{actual_value}")
+    end
 end
 
 And(/^I click on Clear it button$/) do
-  puts ("User clicks on Clear it button ")
+  find_element(id:"menu_clear").click
 end
 
 When(/^I  enter "([^"]*)" to From field$/) do |value|
-  puts ("User entered value #{value}")
+  find_element(id:"header_value_from").send_keys(value)
 end
 
 Then(/^I get "([^"]*)" i n To field$/) do |value|
-  puts ("User sees #{value} in result field ")
+  actual_value = find_element(id:"header_value_to").text
+  if actual_value != value
+    fail ("Expected value is #{value}, but actual value #{actual_value}")
+  end
+end
+
+When(/^I click on From field$/) do
+  find_element(id:"header_value_from").click
+end
+
+And(/^I press "([^"]*)" on soft keyboard$/) do |value|
+  digits = value.split("")
+  digits.each do |key|
+  digit = Integer (key)
+  press_keycode 7 + digit
+    end
+end
+
+When(/^I select "([^"]*)" from left column$/) do |value|
+  find_element(id:"radio_group_from").find_element(xpath: "//android.widget.RadioButton[@text='#{value}']").click
 end
